3c01b338dd8121b40e164826d489056952991bb2
Michael <mikahado@gmail.com>
2022-11-04 16:37:18 -0400

    mock challenge plant shop

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((resp) => resp.json())
+      .then((plantsArray) => {
+        setPlants(plantsArray);
+      });
+  }, []);
+
+  function handleAddPlant(newPlant) {
+    const updatedPlantArray = [...plants, newPlant];
+    setPlants(updatedPlantArray)
+  }
+
   return (
     <main>
-      <NewPlantForm />
+      <NewPlantForm onAddPlant={handleAddPlant}/>
       <Search />
-      <PlantList />
+      <PlantList plants={plants}/>
     </main>
   );
 }
